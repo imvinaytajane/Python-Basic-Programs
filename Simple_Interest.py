731db6f351baf9b76fdcebcb0959c778da6bb7d2
Vinay <imvinaytajane@gmail.com>
2021-09-06 14:57:15 +0530

    deleted year line

--- Simple_Interest.py
+++ Simple_Interest.py
@@ -4,7 +4,7 @@ print("Lets Find Simple Interest!")
 
 p = int(input("Enter Principal Ammount:"))
 r = int(input("Enter Rate of interest:"))
-t = int(input("Time Interval of Interest in Terms of Year:"))
+t = int(input("Time Interval of Interest:"))
 
 pi =(p*r*t)/100
 
