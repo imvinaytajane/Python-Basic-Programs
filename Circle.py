a3b67f470f1081db444e0c01af665a7f8c05b13e
Vinay <imvinaytajane@gmail.com>
2021-09-06 14:46:56 +0530

    circle

---
+++ Circle.py
@@ -0,0 +1,11 @@
+# Made by Vinay on 06 Sept 2021
+# A Program to find circumference & area of Circle
+
+print("YOu Will get circumference & area of a Circle!")
+
+r = float(input("Enter Radius of Circle:"))
+
+pi = 3.14
+
+print("Circumference of Circle is",2*pi*r)
+print("Area of Circle is",pi*r**2)
