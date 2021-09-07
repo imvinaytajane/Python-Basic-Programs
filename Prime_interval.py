4611068cb19ec5eb311ffbb51f4d6bc51429759e
Vinay <imvinaytajane@gmail.com>
2021-09-07 17:52:22 +0530

    almost complete

--- Prime_interval.py
+++ Prime_interval.py
@@ -1,10 +1,15 @@
 # incomplete
 s = int(input("Start"))
 e = int(input("End"))
+prime = None
 
 for i in range(s,e):
     d = range(2,i)
     for m in d:
         if i%m==0:
             break
-        print(i,"is prime")
+        elif prime!=i:
+            print(prime)
+        prime = i
+
+print(prime)
