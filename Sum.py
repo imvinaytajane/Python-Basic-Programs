80e3b68391864fcdb1465f4efda3c83290b83f70
Vinay <imvinaytajane@gmail.com>
2022-01-15 18:37:28 +0530

    modified code

--- Sum.py
+++ Sum.py
@@ -1,4 +1,11 @@
 # Fimding Sum of Two Number
 # Made by Vinay on 05 Sept 2021
-print("Lets Find Sum of Two Numbers!")
-print("Great Sum is:",int(input("Enter First Number:")) + int(input("Enter Second Number:")))
+total = 0
+
+while True:
+    num = input("Enter Number:")
+    if num == 'done':
+        break
+    total = total + float(num)
+
+print("Total Sum is:",total)
